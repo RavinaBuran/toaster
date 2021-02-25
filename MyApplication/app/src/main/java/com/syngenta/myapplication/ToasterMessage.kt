6423cee253db71baaab626453ba534d16855f8f9
Buran Ravina Ramesh INPU <ravina_ramesh.buran@syngenta.com>
2021-02-25 11:47:17 +0530

    bug fixes

--- MyApplication/app/src/main/java/com/syngenta/myapplication/ToasterMessage.kt
+++ MyApplication/app/src/main/java/com/syngenta/myapplication/ToasterMessage.kt
@@ -4,8 +4,8 @@ import android.content.Context
 import android.widget.Toast
 
 
- class ToasterMessage {
-    fun s(c: Context?, message: String?) {
+object ToasterMessage {
+    fun display(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
     }
 }
